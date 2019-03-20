40d193772c761d7fadae1e4f0635bb9016ea72ae
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:22:56 -0400

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a switch block
+
+            Console.WriteLine("Coffee Sizes: 1=Small 2=Medium 3=Large");
+            Console.Write("Please enter your selection:");
+            string str = Console.ReadLine();
+            int cost = 0;
+
+            switch (str)
+            {
+                case "1":
+                case "small":
+                    cost += 25;
+                    break;
+                case "2":
+                case "medium":
+                    cost += 50;
+                    break;
+                case "3":
+                case "large":
+                    cost += 75;
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Please insert {0} cents", cost);
+                    }
+            Console.WriteLine("Thank you for your business!");
+        }
+    }
+}
